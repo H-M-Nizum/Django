275e1e82556dc56cbebfe4660db6a311ecc4da38
H.M.Nizum <hmnizum1714032@gmail.com>
2025-08-21 01:10:47 +0600

    [Digital_medical]- Create Post method

--- digital_medical/doctor/urls.py
+++ digital_medical/doctor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import doctorViewAll, doctorInstanceView
+from .views import doctorViewAll, doctorInstanceView, createDoctorView
 
 urlpatterns = [
     path("list/", doctorViewAll, name='Doctor-List'),
     path("list/<int:pk>", doctorInstanceView, name='Doctor-Instance'),
+    path("create/", createDoctorView, name="create_doctor")
 ]
--- digital_medical/doctor/views.py
+++ digital_medical/doctor/views.py
@@ -3,6 +3,9 @@ from .models import DoctorModel
 from .serializers import DoctorSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse, JsonResponse
+import io
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
@@ -34,3 +37,31 @@ def doctorInstanceView(request, pk):
     
     except DoctorModel.DoesNotExist:
         return JsonResponse({'error': 'Doctor not found'}, status=404)
+
+
+@csrf_exempt
+def createDoctorView(request):
+    # Json data or request body data
+    json_data = request.body
+    print("json data : ", json_data)
+
+    # json to Stream Data
+    stream_data = io.BytesIO(json_data)
+    print("stream Data : ", stream_data)
+
+    # Stream to python data
+    python_data = JSONParser().parse(stream_data)
+    print("python data : ", python_data)
+
+    # Python to complex data
+    serializer_data = DoctorSerializer(data=python_data)
+    print("Serializer Data : ", serializer_data)
+
+    # Validation check
+    if serializer_data.is_valid():
+        serializer_data.save()
+        response_data = JSONRenderer().render({"msg" : "Successfully Create a Doctor instance"})
+    else:
+        response_data = JSONRenderer().render({"msg" : "Validation Error", "error" : serializer_data.errors})
+    return HttpResponse(response_data, content_type='application/json')
+
