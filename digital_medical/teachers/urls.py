5e2ae6f8fe7c6f3f029fcda831b4078d2c97c9ff
H.M.Nizum <hmnizum1714032@gmail.com>
2025-08-24 23:56:11 +0600

    [Digital_medical]-  Create  class-based views ModelViewSet

--- digital_medical/teachers/urls.py
+++ digital_medical/teachers/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
-from .views import TeacherListCreateView, TeacherRetriveUpdateDestroyView, TeacherRetriveDestroyView, TeacherRetriveUpdateView
+from django.urls import path, include
+from .views import TeacherListCreateView, TeacherRetriveUpdateDestroyView, TeacherRetriveDestroyView, TeacherRetriveUpdateView, TeacherModelViewSetView
+from rest_framework.routers import DefaultRouter
+
+# Router for ModelViewSet
+router = DefaultRouter()
+router.register('teacher-viewset', TeacherModelViewSetView, basename='teacher')
+
 
 urlpatterns = [
+    # Old class-based APIViews
     path('teacher/', TeacherListCreateView.as_view(), name='teacher-list-create'),
     path('teacher/<int:pk>', TeacherRetriveUpdateDestroyView.as_view(), name='teacher-retrive-update-destroy'),
     path('teacher/retrive-destroy/<int:pk>', TeacherRetriveDestroyView.as_view(), name='teacher-retrive-destroy'),
     path('teacher/retrive-update/<int:pk>', TeacherRetriveUpdateView.as_view(), name='teacher-retrive-update'),
     
+
+    # ModelViewSet routes
+    path('', include(router.urls)),
 ]
--- digital_medical/teachers/views.py
+++ digital_medical/teachers/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import TeacherModel
 from .serializers import TeacherSerializers
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView, RetrieveUpdateAPIView
-
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
 
@@ -25,5 +25,10 @@ class TeacherRetriveDestroyView(RetrieveDestroyAPIView):
 
 # View for Retrive and Update
 class TeacherRetriveUpdateView(RetrieveUpdateAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializers
+
+# View for List, Create, Retrive, Update And Destroy using ModelViewset
+class TeacherModelViewSetView(ModelViewSet):
     queryset = TeacherModel.objects.all()
     serializer_class = TeacherSerializers
