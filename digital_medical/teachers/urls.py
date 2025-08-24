d47cd09eafc9eaacf66bf995c28f92981df80ea3
H.M.Nizum <hmnizum1714032@gmail.com>
2025-08-24 23:37:39 +0600

    [Digital_medical]-  Create Using generic class-based views - (ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView, RetrieveUpdateAPIView)

--- digital_medical/teachers/urls.py
+++ digital_medical/teachers/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import TeacherListCreateView, TeacherRetriveUpdateDestroyView, TeacherRetriveDestroyView, TeacherRetriveUpdateView
 
 urlpatterns = [
+    path('teacher/', TeacherListCreateView.as_view(), name='teacher-list-create'),
+    path('teacher/<int:pk>', TeacherRetriveUpdateDestroyView.as_view(), name='teacher-retrive-update-destroy'),
+    path('teacher/retrive-destroy/<int:pk>', TeacherRetriveDestroyView.as_view(), name='teacher-retrive-destroy'),
+    path('teacher/retrive-update/<int:pk>', TeacherRetriveUpdateView.as_view(), name='teacher-retrive-update'),
     
 ]
--- digital_medical/teachers/views.py
+++ digital_medical/teachers/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import TeacherModel
+from .serializers import TeacherSerializers
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView, RetrieveUpdateAPIView
 
 # Create your views here.
+
+
+# View for List and Create
+class TeacherListCreateView(ListCreateAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializers
+
+
+# View for Retrive, Update And Destroy
+class TeacherRetriveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializers
+
+# View for Retrive and Destroy
+class TeacherRetriveDestroyView(RetrieveDestroyAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializers
+
+
+# View for Retrive and Update
+class TeacherRetriveUpdateView(RetrieveUpdateAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializers
