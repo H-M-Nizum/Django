58e008b06ad72a5ffc6f652be68e783c8f7a56b3
H.M.Nizum <hmnizum1714032@gmail.com>
2025-08-31 22:14:51 +0600

    [Digital_medical]- Create Access And Refresh Token for register and login time

--- digital_medical/accounts/views.py
+++ digital_medical/accounts/views.py
@@ -4,15 +4,29 @@ from rest_framework import status
 from .serializers import UserRegisterSerializers, UserLoginSerializers
 from django.contrib.auth import authenticate
 from .renderers import UserRenderers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
+# Creating tokens manually
+def get_tokens_for_user(user):
+    if not user.is_active:
+      raise AuthenticationFailed("User is not active")
+
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 # View for Register
 class UserRegistrationViews(APIView):
     renderer_classes = [UserRenderers]
     def post(self, request, format=None):
         serializer_data = UserRegisterSerializers(data = request.data)
         if serializer_data.is_valid(raise_exception=True):
-            serializer_data.save()
-            return Response({'status': 'success', 'msg': 'User Registration Successfully Complete', 'data' : serializer_data.data}, status=status.HTTP_201_CREATED)
+            user = serializer_data.save()
+            token = get_tokens_for_user(user)
+            return Response({'status': 'success', 'msg': 'User Registration Successfully Complete', 'data' : serializer_data.data, 'token': token}, status=status.HTTP_201_CREATED)
         return Response({'status': 'error', 'msg' : 'Failed to Register User', 'error' : serializer_data.errors}, status=status.HTTP_400_BAD_REQUEST)
     
 
@@ -27,7 +41,8 @@ class UserLoginViews(APIView):
             
             user = authenticate(email=email, password=password)
             if user is not None:
-                return Response({'status': 'success', 'msg': 'User Login Successfully Complete'}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+                return Response({'status': 'success', 'msg': 'User Login Successfully Complete', 'token': token}, status=status.HTTP_200_OK)
             else:
                 return Response({'status': 'error', 'msg': 'Failed to User Login', 'error' : {'non_field_error' : 'Email or Password is not valid'}}, status=status.HTTP_404_NOT_FOUND)
 
