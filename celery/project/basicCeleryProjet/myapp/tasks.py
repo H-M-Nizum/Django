22442d4bc9068fd9d2b77be295d2d4ed402202fa
H.M.Nizum <hmnizum1714032@gmail.com>
2025-09-08 21:48:27 +0600

    [Celery] - django-celery-beat - Database-backed Periodic Tasks - setup in setting.py but not recomended

--- celery/project/basicCeleryProjet/myapp/tasks.py
+++ celery/project/basicCeleryProjet/myapp/tasks.py
@@ -10,3 +10,8 @@ def add(x, y):
 def sub(x, y):
     time.sleep(20)
     return f"Then Subtraction of {x} and {y} is : {x - y}"
+
+@shared_task
+def clear_session_cache(id):
+    print(f"Session Cache Clear for : {id}")
+    return id
